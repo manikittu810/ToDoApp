0221becbfa3eb5ff6d155b9285981db432053cc3
Manikrishna Sanganabatla <msanganabatla@csu.fullerton.edu>
2023-08-06 22:39:41 -0700

    added ModelMap and used @RequestParam

--- src/main/java/ToDoApp/myFirstWebApp/login/LoginController.java
+++ src/main/java/ToDoApp/myFirstWebApp/login/LoginController.java
@@ -1,25 +1,22 @@
 package ToDoApp.myFirstWebApp.login;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import static org.slf4j.LoggerFactory.getLogger;
 
 @Controller
 public class LoginController {
-//    private Logger logger= LoggerFactory.getLogger(getClass());
-    @RequestMapping("login")
-    public String loginPage(@RequestParam String name, ModelMap model){
-        model.put("name",name);
-//        logger.debug("I want to print this at debug level {}" ,name);
-//        logger.info("I want to print this at info level {}" ,name);
-//        logger.warn("I want to print this at warn level {}" ,name);
-        System.out.println("Request Param is " + name); //not recommended for production code
-
+    @RequestMapping(value="login",method = RequestMethod.GET)
+    public String loginPage(){
         return "login";
+    }
+    @RequestMapping(value ="login",method=RequestMethod.POST)
+    public String welcomePage(@RequestParam String name, @RequestParam String password, ModelMap model){
+        model.put("name",name);
+        model.put("password",password);
+
+        return "welcome";
     }
 }
