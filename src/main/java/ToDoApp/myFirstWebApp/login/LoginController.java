5c05fbbcc3fd9509c72dd5ff74db4583a80e5853
Manikrishna Sanganabatla <msanganabatla@csu.fullerton.edu>
2023-08-06 19:26:38 -0700

    Added logging

--- src/main/java/ToDoApp/myFirstWebApp/login/LoginController.java
+++ src/main/java/ToDoApp/myFirstWebApp/login/LoginController.java
@@ -1,16 +1,25 @@
 package ToDoApp.myFirstWebApp.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 @Controller
 public class LoginController {
+//    private Logger logger= LoggerFactory.getLogger(getClass());
     @RequestMapping("login")
     public String loginPage(@RequestParam String name, ModelMap model){
         model.put("name",name);
+//        logger.debug("I want to print this at debug level {}" ,name);
+//        logger.info("I want to print this at info level {}" ,name);
+//        logger.warn("I want to print this at warn level {}" ,name);
         System.out.println("Request Param is " + name); //not recommended for production code
+
         return "login";
     }
 }
