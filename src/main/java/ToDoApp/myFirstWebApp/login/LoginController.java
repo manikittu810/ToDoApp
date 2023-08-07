14e1fc632400fac3c87b04370cef8cc9e52719fb
Manikrishna Sanganabatla <msanganabatla@csu.fullerton.edu>
2023-08-06 17:47:08 -0700

    added Model ,used model.put method

--- src/main/java/ToDoApp/myFirstWebApp/login/LoginController.java
+++ src/main/java/ToDoApp/myFirstWebApp/login/LoginController.java
@@ -1,12 +1,16 @@
 package ToDoApp.myFirstWebApp.login;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
     @RequestMapping("login")
-    public String loginPage(){
+    public String loginPage(@RequestParam String name, ModelMap model){
+        model.put("name",name);
+        System.out.println("Request Param is " + name); //not recommended for production code
         return "login";
     }
 }
