f4196dc5302e8b79ab8612252a282d4b3b980787
Manikrishna Sanganabatla <msanganabatla@csu.fullerton.edu>
2023-08-12 14:52:55 -0700

    cleaned up the code for implementing spring security

---
+++ src/main/java/ToDoApp/myFirstWebApp/login/welcomeLoginController.java
@@ -0,0 +1,19 @@
+package ToDoApp.myFirstWebApp.login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+@Controller
+@SessionAttributes("name")
+public class welcomeLoginController {
+
+@RequestMapping(value="/",method = RequestMethod.GET)
+public String goToWelcomePage(ModelMap model){
+    model.put("name","smk");
+    return "welcome";
+}
+
+}
