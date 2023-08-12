c65c39a96a62559cd33ee8865a8a8d5cf9c976bd
Manikrishna Sanganabatla <msanganabatla@csu.fullerton.edu>
2023-08-12 15:34:40 -0700

    Configuring Spring Security with Custom User and Password Encoder

---
+++ src/main/java/ToDoApp/myFirstWebApp/security/springSecurityConfiguration.java
@@ -0,0 +1,30 @@
+package ToDoApp.myFirstWebApp.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.function.Function;
+
+@Configuration
+public class springSecurityConfiguration {
+    @Bean
+public InMemoryUserDetailsManager createUserDetailManager(){
+        Function<String, String> passwordEncoder=
+                input-> passwordEncoder().encode(input);
+        UserDetails userDetails=User.builder().passwordEncoder(passwordEncoder)
+                .username("Smk123")
+                .password("smk")
+                .roles("USER","ADMIN").build();
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+@Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+}
+
+}
